c0c5bade7d1c251bb8828a24c5206e6956137147
Kaori <xiaxulizhaiteng@gmail.com>
2018-11-27 22:10:04 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  root to: 'blogs#index'
+  root to: 'blogs#top'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
