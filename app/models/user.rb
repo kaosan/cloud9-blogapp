d275b459f4b2ec688e7dec77489d7ff1218b8c0a
Kaori <xiaxulizhaiteng@gmail.com>
2019-01-08 21:53:08 +0900

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  mount_uploader :icon, IconUploader
+  mount_uploader :icon, ImageUploader
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255},   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
